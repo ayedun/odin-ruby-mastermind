c75f13a6df22b4a0815d579dd4989fa9e016006f
ayedun <88404572+ayedun@users.noreply.github.com>
2023-01-28 13:13:38 -0800

    Attempt to match answers to guess

--- main.rb
+++ main.rb
@@ -12,16 +12,30 @@ class Game
         @answer_board = Array.new(4, "_")
     end
 
-    def display_code_board
+    def displayCodeBoard
         @code_board.each do |value|
             print "#{value} "
         end
         puts "\n"
     end
-    def create_code
+    def createCode
         puts "Hello Code mastermind! Please enter your code(secretly!)"
         puts "Please seperate the color codes with spaces: "
         @answer_board = gets.chomp.split(" ")
+    end
+    def turn(player1,player2)
+        puts "#{player1}, please enter your guess at the secret code.\n Enter all 4 color names seperated by spaces: "
+        player1_guess = gets.chomp.split(" ")
+        player1_guess.each_with_index do |value, index|
+            if answer_board.any? { |answer| answer == value}
+                player1_guess.each_with_index do |actual_guess, actual_index|
+                    answer_board.each_with_index do |actual_answer, actual_index|
+                        #trying to set up all answer to equal player1
+                end
+            end
+        end
+        
+        
     end
 
 end
@@ -47,5 +61,6 @@ player2 = nil
 player1 = Players.new(getPlayerName(player1, player2))
 player2 = Players.new(getPlayerName(player1, player2))
 game = Game.new
-game.create_code
-game.display_code_board
+game.createCode
+game.displayCodeBoard
+game.turn(player1, player2)
